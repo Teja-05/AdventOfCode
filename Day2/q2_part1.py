cc04b94f50c7b827365db59e2f3a6b48acd9a233
Teja-05 <132153842+Teja-05@users.noreply.github.com>
2024-01-24 21:35:25 +0530

    Add files via upload

---
+++ Day2/q2_part1.py
@@ -0,0 +1,35 @@
+with open('games.txt', 'r') as file:
+    ip = file.read()
+
+l = ip.split('\n')
+
+total = 0
+d = {'red':12, 'green':13, 'blue':14}
+
+for line in l:
+    game, trials = line.split(':')
+    gameid = game.split(' ')[-1]
+    trial = trials.split(';') #get each trial for each game
+
+    arr = []
+
+    #check for each trial if the count is less than  the limit for all colors
+    for t in trial:
+        curr = {'red':0, 'green':0, 'blue':0}
+        cubeset = t.split(',')
+        for cube in cubeset:
+            cube = cube.lstrip(' ')
+            qty, color = cube.split(' ')
+            curr[color] += int(qty)
+
+        check = all((d[color]>=qty for color, qty in curr.items()))
+        arr.append(check)
+
+    if sum(arr)==len(arr): #when all trials of a game satisfy the limit
+        total += int(gameid)
+
+print(total)
+
+
+
+
---
+++ Day2/q2_part2.py
@@ -0,0 +1,40 @@
+with open('games.txt', 'r') as file:
+    ip = file.read()
+
+l = ip.split('\n')
+
+total = 0
+
+for line in l:
+    game, trials = line.split(':')
+    gameid = game.split(' ')[-1]
+    trial = trials.split(';') #get each trial for each game
+
+    arr = []
+    rmax, gmax, bmax = -1, -1, -1
+
+    #find the maximum number of cubes for each color in a game across all trials
+    for t in trial:
+        curr = {'red':0, 'green':0, 'blue':0}
+        cubeset = t.split(',')
+        for cube in cubeset:
+            cube = cube.lstrip(' ')
+            qty, color = cube.split(' ')
+            curr[color] += int(qty)
+
+        for color, qty in curr.items(): #update max values
+            if color=='red' and qty>rmax:
+                rmax = qty
+            if color=='green' and qty>gmax:
+                gmax = qty
+            if color=='blue' and qty>bmax:
+                bmax = qty
+
+    power = rmax*gmax*bmax
+    total += power
+
+print(total)
+
+
+
+
