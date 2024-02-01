a4de682d47f8d6ff6f0a5f53fc16987cf4faa740
Teja-05 <132153842+Teja-05@users.noreply.github.com>
2024-02-01 21:47:46 +0530

    Add files via upload

---
+++ Day9/q9_part1.py
@@ -0,0 +1,24 @@
+with open('report.txt', 'r') as f:
+    ip = f.read()
+
+l = ip.split('\n')
+
+total = 0
+histories = []
+
+def func(arr):
+    diff = []
+    if arr.count(0)==len(arr): #if all are zeroes
+        return 0
+    for i in range(len(arr)-1):
+        diff.append(arr[i+1]-arr[i]) #find difference until all are zeroes
+    return arr[-1] + func(diff)
+
+for line in l:
+    histories.append([int(x) for x in line.split(' ')])
+
+for h in histories:
+    nextval = func(h) #next value for each history in report
+    total += nextval
+
+print(total)
